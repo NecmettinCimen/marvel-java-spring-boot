588ee7b1e61a6f1fc5c745e866edfd522d29aa50
Necmettin <necmettin.dev@gmail.com>
2025-06-26 14:20:49 +0300

    fix

--- src/main/java/xyz/necmettincimen/marvel/marvel/adapter/in/web/UserController.java
+++ src/main/java/xyz/necmettincimen/marvel/marvel/adapter/in/web/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import xyz.necmettincimen.marvel.marvel.application.service.UserService;
