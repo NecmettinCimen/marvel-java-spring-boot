4abad2cf4dfc9ffac38a9c2d993e719a58312513
Necmettin <necmettin.dev@gmail.com>
2025-07-28 10:03:39 +0300

    Enhance CORS configuration in SecurityConfig; allow credentials and specify allowed origins, methods, and headers

--- src/main/java/xyz/necmettincimen/marvel/marvel/common/SecurityConfig.java
+++ src/main/java/xyz/necmettincimen/marvel/marvel/common/SecurityConfig.java
@@ -7,8 +7,15 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -17,6 +24,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers(
                                 "/swagger-ui/**",
@@ -24,12 +32,22 @@ public class SecurityConfig {
                                 "/api/users/public/**")
                         .permitAll().anyExchange().authenticated())
                 .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-
                 .build();
     }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://marvel-java-app.necmettincimen.xyz", "https://nec-ocelot-gateway.necmettincimen.xyz")); // İzin verilen kaynaklar
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // İzin verilen HTTP metotları
+        configuration.setAllowedHeaders(Arrays.asList("*")); // İzin verilen başlıklar
+        configuration.setAllowCredentials(true); // Kimlik bilgilerine (cookie, HTTP authentication) izin ver
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Tüm path'ler için geçerli
+        return source;
     }
 }
